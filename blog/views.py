f42351bcc2d7f14bbf05cb033a5373521b596e74
Mojojojo9975 <moizkhan9975@gmail.com>
2024-05-12 19:09:09 +0500

    Template render error

--- blog/views.py
+++ blog/views.py
@@ -14,6 +14,6 @@ def post_detail(request,year,month,day,post):
                                 publish__year=year,
                                 publish__month=month,
                                 publish__day=day)
-    return render(request,'post/detail.html',{'post': post})
+    return render(request,'post/post_detail.html',{'post': post})
 
 
